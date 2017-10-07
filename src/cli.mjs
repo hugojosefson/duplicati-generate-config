9e9d616923034e72859093877dec2b1fff7a85ac
Hugo Josefson <hugo@josefson.org>
2017-10-07 15:30:54 +0200

    Call yargs.parse() instead of accessing yargs.argv getter.

--- src/cli.mjs
+++ src/cli.mjs
@@ -3,12 +3,9 @@ import termSize from 'term-size'
 import generate from './cli-commands/generate'
 import { readFile, writeFile, generateWriteSpecs } from './api'
 
-const yargsChain = yargs
+yargs
   .command(generate({ readFile, writeFile, generateWriteSpecs }))
   .demandCommand()
   .help()
   .wrap(termSize().columns)
-
-/* Runs yargs, strange as it may look: */
-// noinspection BadExpressionStatementJS
-yargsChain.argv // eslint-disable-line no-unused-expressions
+  .parse()
